c4a15cfefbc93dcb9c13da09bcb8e6a05dd52324
sylvain <sylvain@kinetix.tech>
2024-01-05 17:49:18 +0100

    [ADD] Minimalistic cli project + github job

---
+++ CodeWalker.Console/ConsoleArgumentException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeWalker.Console
+{
+	[Serializable]
+	public class ConsoleArgumentException : Exception
+	{
+		public ConsoleArgumentException() { }
+		public ConsoleArgumentException(string message) : base(message) { }
+		public ConsoleArgumentException(string message, Exception inner) : base(message, inner) { }
+		protected ConsoleArgumentException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
---
+++ CodeWalker.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using CodeWalker.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace CodeWalker.Console
+{
+	internal class Program
+	{
+
+		const string USAGE =
+		#region USAGE
+@"Usage:
+cw_cli source output
+";
+		#endregion
+
+		private static void Main(string[] args)
+		{
+			try
+			{
+				Core(args);
+				Environment.ExitCode = 0;
+			}
+			catch (ConsoleArgumentException)
+			{
+				Environment.ExitCode = 1;
+				return;
+			}
+			catch (Exception e)
+			{
+				Environment.ExitCode = 2;
+				throw new Exception("", e);
+			}
+		}
+	}
+}
